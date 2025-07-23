a6227e68ee1f9c68595c6b4f16dbc19b13783bc6
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2025-07-23 14:58:54 +0100

    Update ai-medical-chatbot.py

--- ai-medical-chatbot.py
+++ ai-medical-chatbot.py
@@ -1,1 +1,9 @@
 pip install pandas scikit-learn sentence-transformers
+import pandas as pd
+
+df = pd.read_csv("ai-medical-chatbot.csv", on_bad_lines='skip', engine='python')
+
+
+df["full_question"] = df["Description"].fillna('') + " " + df["Patient"].fillna('')
+questions = df["full_question"].tolist()
+answers = df["Doctor"].fillna('').tolist()
