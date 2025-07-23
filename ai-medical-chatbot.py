7ec755a0cc6139fc132fa1e6a780ea0de349b318
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2025-07-23 14:59:20 +0100

    Update ai-medical-chatbot.py

--- ai-medical-chatbot.py
+++ ai-medical-chatbot.py
@@ -7,3 +7,10 @@ df = pd.read_csv("ai-medical-chatbot.csv", on_bad_lines='skip', engine='python')
 df["full_question"] = df["Description"].fillna('') + " " + df["Patient"].fillna('')
 questions = df["full_question"].tolist()
 answers = df["Doctor"].fillna('').tolist()
+
+from sentence_transformers import SentenceTransformer
+
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+question_embeddings = model.encode(questions, show_progress_bar=True)
